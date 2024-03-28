437d55f86950709b999d2c7abe6b3fe757ddb644
Brian Nolan <bnolan001@gmail.com>
2024-03-28 12:05:22 +0000

    Mar 28, 2024, 12:05 PM

--- script.js
+++ script.js
@@ -1,13 +1,12 @@
 let text = "Hello";
-function updateDiv(id){
-    let divElement = document.getElementById(id);
-    if(divElement.innerText == ""){
-        divElement.innerText = text;
-    }
-    if (text== "Hi"){
-        text = "Hola";
-    }
-    else {
-        text = "Hallo";
-    }
+function updateDiv(id) {
+  let divElement = document.getElementById(id);
+  if (divElement.innerText == "") {
+    divElement.innerText = text;
+  }
+  if (text == "Hello") {
+    text = "Hola";
+  } else {
+    text = "Hallo";
+  }
 }
