6e18163791c3c3ac606d8714cc09c9f608cf4ef6
Gabriella <156961033+gabriellaM7@users.noreply.github.com>
2024-04-03 00:45:50 +0000

    Apr 3, 2024, 12:45 AM

--- script.js
+++ script.js
@@ -2,7 +2,6 @@
 function divClicked(id){
   let divElement = document.getElementById(id);
   if (divElement.innerText == ""){
- 
   console.log("Element: ", divElement innerText);
   divElement.innertext = "x"; 
   }else {
