bd6dc0871a9e06288d7718500c19ba8cca6d6930
Gabriella <156961033+gabriellaM7@users.noreply.github.com>
2024-04-03 00:44:11 +0000

    Apr 3, 2024, 12:44 AM

--- script.js
+++ script.js
@@ -1,32 +1,11 @@
-// Use this variable to track what should be displaed in the DIV elements
-let text = "Hello";
-
-/*
-  Callback function for when the user clicks on a div element.  The ID
-  of the div element is passed back so we know which one we are working with.
-*/
-function updateDiv(id) {
-  // Grab the element from the DOM by the id
-  let divElement = document.getElementById(id);
-
-  // If we haven't already set the content inside the element then go ahead and
-  // set it to whatever is currently stored in the text variable
-  if (divElement.innerText == "") {
-    divElement.innerText = text;
-  }
-
-  // Update the text variable so we show something different in each div element
-  if (text == "Hello") {
-    text = "Hola";
-  } else {
-    text = "Hallo";
-  }
-}
-
-
-
 
 function divClicked(id){
   let divElement = document.getElementById(id);
-  console.log("Element: ", divElement);
+  if (divElement.innerText == ""){
+ 
+  console.log("Element: ", divElement innerText);
+  divElement.innertext = "x"; 
+  }else {
+    console.log("Skipped setting the inner text");
+  }
 }
