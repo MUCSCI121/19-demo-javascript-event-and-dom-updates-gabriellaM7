b43b3ed999868da3f0b370faf858db62efa24b90
Gabriella <156961033+gabriellaM7@users.noreply.github.com>
2024-04-03 00:49:01 +0000

    v

--- script.js
+++ script.js
@@ -6,6 +6,12 @@ function divClicked(id){
   if (divElement.innerText == ""){
   console.log("Element set to: ", divElement innerText);
   divElement.innertext = "x"; 
+  if (nextTextToDisplay == "a"){
+    nextTextToDisplay = "b";
+  }
+  else if (nextTextToDisplay == "b"){
+    
+  }
   }else {
     console.log("Skipped setting the inner text");
   }
