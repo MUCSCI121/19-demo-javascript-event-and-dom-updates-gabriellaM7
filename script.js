36b5f9b0755b05d58077c721c887a9277c588a63
Gabriella <156961033+gabriellaM7@users.noreply.github.com>
2024-04-03 00:47:24 +0000

    Apr 3, 2024, 12:47 AM

--- script.js
+++ script.js
@@ -4,7 +4,7 @@ let nextTextToDisplay = "a";
 function divClicked(id){
   let divElement = document.getElementById(id);
   if (divElement.innerText == ""){
-  console.log("Element: ", divElement innerText);
+  console.log("Element set to: ", divElement innerText);
   divElement.innertext = "x"; 
   }else {
     console.log("Skipped setting the inner text");
